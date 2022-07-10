5251ee96dffcd34ab5c61ecb667c10d3c18d67a4
charlesdzadu <dkccharlie@gmail.com>
2022-07-10 20:05:17 +0000

    save paygate transaction to database

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 
-
-const app = express();
 admin.initializeApp(functions.config().firebase)
 
 exports.getUserByEmail = functions.https.onRequest((request, response) => {
@@ -18,7 +16,40 @@ exports.getUserByEmail = functions.https.onRequest((request, response) => {
 });
 
 exports.paygateCallback = functions.https.onRequest((request, response) => {
+    var body = request.body;
+    var query = request.query;
     if (request.method == "POST") {
-        return "C'est un post request";
+        var data = {
+            "txReference": body['tx_reference'],
+            "identifier": body['identifier'],
+            "paymentReference": body['payment_reference'],
+            "amount": body['amount'],
+            "datetime": body['datetime'],
+            "payementMethod": body['payment_method'],
+            "phoneNumber": body['phone_number'],
+            "user_agent_api": request.headers["user-agent"],
+            "x-appengine-citylatlong": request.headers['x-appengine-citylatlong'],
+            "x-appengine-city": request.headers['x-appengine-city'],
+            "x-appengine-country": request.headers['x-appengine-country'],
+            "x-appengine-user-ip": request.headers['x-appengine-user-ip'],
+
+
+        };
+
+        admin.firestore().collection("paymentsTransactions").add(data).then((res) => {
+            response.send(request.headers);
+
+        }).catch((e) => {
+            console.log("erruer", e);
+            response.send("Une erreur est survenue");
+        })
+
+
+
+    } else {
+        var data = {
+            "message": "Les requête avec ce verbe ne sont pas autorisé",
+        };
+        response.send(data);
     }
 })
