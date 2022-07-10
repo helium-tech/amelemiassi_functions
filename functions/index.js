270f9fdc38578bc8ec837a98f134cffe5d8e185d
charlesdzadu <dkccharlie@gmail.com>
2022-07-10 09:05:39 +0000

    Get user by email work

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,17 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase)
+
+exports.getUserByEmail = functions.https.onRequest((request, response) => {
+    console.log("query data", request.query);
+    admin.auth().getUserByEmail("charlesdzadu@gmail.com").then((res) => {
+        console.log("succesfully get user :", res.toJSON())
+        response.send(res.toJSON());
+    }).catch((error) => {
+        console.log("an error occured when getting data", error)
+    })
+});
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
